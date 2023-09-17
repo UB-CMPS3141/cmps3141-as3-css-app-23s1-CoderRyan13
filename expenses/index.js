9630903b7553f1d6f13843117725e1bc80742be5
Ryan Armstrong <armstrongryan11@gmail.com>
2023-09-17 12:56:06 -0600

    Ways in for exercise 2

--- expenses/index.js
+++ expenses/index.js
@@ -8,7 +8,20 @@ import { createApp } from "https://mavue.mavo.io/mavue.js";
 
 globalThis.app = createApp({
 	data: {
-		expenses: []
+		expenses: [],
+		personA: "Neo",
+		personB: "Trinity",
+		joint: "Joint",
+		totalPersonAOwes: 0,
+		totalPersonBOwes: 0,
+		
+		form: {
+			payer: "",
+			payingTo: "",
+			amount: 0.00.toFixed(2),
+			description: "",
+			pDate: ""
+		}
 	},
 
 	methods: {
@@ -30,6 +43,48 @@ globalThis.app = createApp({
 			};
 
 			return amount * rates[to] / rates[from];
+		},
+
+		addPayment() {
+			const payer = this.form.payer;
+			const payingTo = this.form.payingTo;
+			const amount = this.form.amount;
+			const description = this.form.description;
+			const pDate = this.form.pDate;
+
+			let personAOwes = 0;
+			if(payer===this.personB && payingTo===this.personA) {
+				personAOwes = this.form.amount;
+			} else if(payer===this.personB && payingTo===this.joint || payer===this.joint && payingTo===this.personA) {
+				personAOwes = this.form.amount/2;
+			} else {
+				personAOwes = 0;
+			}
+
+			let personBOwes = 0;
+			if(payer===this.personA && payingTo===this.personB) {
+				personBOwes = this.form.amount;
+			} else if(payer===this.personA && payingTo===this.joint || payer===this.joint && payingTo===this.personB) {
+				personBOwes = this.form.amount/2;
+			} else {
+				personBOwes = 0;
+			}
+
+			this.totalPersonAOwes += personAOwes;
+
+			this.totalPersonBOwes += personBOwes;
+
+			this.expenses.push({
+				payer,
+				payingTo,
+				amount,
+				description,
+				pDate,
+				personAOwes,
+				personBOwes
+			});
+			this.form={};
+			//console.log(this.expenses);
 		}
 	},
 
